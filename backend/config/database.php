1de11c27c688cc2c834db20f8c1cb55d02c9d940
tienthanhr <nguyenthanh123426@gmail.com>
2025-12-09 01:11:29 +1300

    Fix: Disable ONLY_FULL_GROUP_BY sql mode

--- backend/config/database.php
+++ backend/config/database.php
@@ -69,6 +69,7 @@ class Database {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
             ];
             
             $this->connection = new PDO($dsn, $username, $password, $options);
